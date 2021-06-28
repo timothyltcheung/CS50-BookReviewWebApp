2798d31a1c41f20c2209b0171fcd88235f02b258
Tim <timothyltcheung@gmail.com>
2021-06-27 23:54:40 -0400

    Cleaning up per PEP8

--- application.py
+++ application.py
@@ -27,84 +27,85 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 
-@app.route("/", methods = ["POST", "GET"])
+@app.route("/", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
         newusername = request.form.get("newusername")
         newpassword = request.form.get("newpassword")
         try:
-            db.execute("INSERT INTO users (username, password) VALUES (:username, :password)",
-                    {"username": newusername, "password": newpassword})
+            db.execute("INSERT INTO users (username, password) VALUES (:username, :password)", {"username": newusername, "password": newpassword})
             db.commit()
             return render_template("login.html", message="Registration Successful, Please Login")
         except ValueError:
             render_template("error.html", message="Registration Unsuccessful")
     if request.method == "GET":
         if session:
-            return render_template("home.html", username = session["user_id"][1])
+            return render_template("home.html", username=session["user_id"][1])
         else:
             return render_template("login.html", message="Please Login")
+
 
 @app.route("/register")
 def register():
     return render_template("register.html")
 
-@app.route("/home", methods=["GET","POST"])
+
+@app.route("/home", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
-        user = db.execute("SELECT * FROM users WHERE username = :username AND password = :password",
-        {"username": username, "password": password}).fetchone()
+        user = db.execute("SELECT * FROM users WHERE username = :username AND password = :password", {"username": username, "password": password}).fetchone()
         if user:
             session["user_id"] = user
         else:
             return render_template("error.html", message="Login Unsuccessful")
     if session:
-        return render_template("home.html", username = session["user_id"][1])
+        return render_template("home.html", username=session["user_id"][1])
     else:
         return render_template("login.html", message="Please Login")
 
-@app.route("/logout", methods = ["GET"])
+
+@app.route("/logout", methods=["GET"])
 def logout():
     session.clear()
     return render_template("login.html", message="Please Login")
 
+
 @app.route("/books", methods=["POST"])
 def books():
     identifier = request.form.get("input")
-    books = db.execute("SELECT * FROM books WHERE title LIKE :input OR author LIKE :input OR isbn LIKE :input",
-    {"input": '%'+ identifier +'%'}).fetchall()
+    books = db.execute("SELECT * FROM books WHERE title LIKE :input OR author LIKE :input OR isbn LIKE :input", {"input": '%' + identifier + '%'}).fetchall()
     if books:
         return render_template('books.html', books=books)
     else:
-        return render_template('error.html', message ="Book not found"),404
+        return render_template('error.html', message="Book not found"), 404
 
 
-@app.route("/books/<string:isbn>", methods=["GET","POST"])
+@app.route("/books/<string:isbn>", methods=["GET", "POST"])
 def book(isbn):
     if request.method == "POST":
-        if db.execute("SELECT * FROM reviews WHERE isbn = :isbn AND user_id = :user_id", {"isbn": isbn, "user_id":session["user_id"][0]}).fetchone():
-            return render_template('error.html', message = "Already Reviewed this Book")
+        if db.execute("SELECT * FROM reviews WHERE isbn = :isbn AND user_id = :user_id", {"isbn": isbn, "user_id": session["user_id"][0]}).fetchone():
+            return render_template('error.html', message="Already Reviewed this Book")
         review_score = request.form.get("review_score")
         review = request.form.get("review")
         try:
-            db.execute("INSERT INTO reviews (user_id, isbn, review_score, review) VALUES (:user_id, :isbn, :review_score, :review)",
-            {"user_id":session["user_id"][0], "isbn": isbn, "review_score": review_score, "review": review})
+            db.execute("INSERT INTO reviews (user_id, isbn, review_score, review) VALUES (:user_id, :isbn, :review_score, :review)", {"user_id": session["user_id"][0], "isbn": isbn, "review_score": review_score, "review": review})
             db.commit()
         except:
-            return render_template('error.html', message = "Score must be between 1-5")
+            return render_template('error.html', message="Score must be between 1-5")
     book = db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": isbn}).fetchone()
     reviews = db.execute("SELECT * FROM reviews LEFT JOIN users ON reviews.user_id = users.id WHERE isbn =:isbn", {"isbn": isbn})
     res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "Eei1Pig7Rov3f6dMWX9A", "isbns": isbn})
     # Call to Goodreads API
     res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": GoodreadsKey, "isbns": isbn})
     goodreadsData = [res.json()['books'][0]['average_rating'], res.json()['books'][0]['work_ratings_count']]
-    return render_template('book.html', book = book, reviews = reviews, goodreads = goodreadsData)
+    return render_template('book.html', book=book, reviews=reviews, goodreads=goodreadsData)
 
-@app.route("/api/<string:isbn>", methods = ["GET"])
+
+@app.route("/api/<string:isbn>", methods=["GET"])
 def api(isbn):
-    book = db.execute("SELECT * FROM books WHERE isbn =:isbn",{"isbn": isbn}).fetchone()
+    book = db.execute("SELECT * FROM books WHERE isbn =:isbn", {"isbn": isbn}).fetchone()
     if book:
         reviews = db.execute("SELECT COUNT(review_score), CAST(AVG(review_score) AS FLOAT) FROM reviews WHERE isbn =:isbn", {"isbn": isbn}).fetchone()
         return jsonify({
--- import.py
+++ import.py
@@ -7,14 +7,15 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
+
 def main():
     data = open('books.csv')
     reader = csv.reader(data)
     for ISBN, title, author, year in reader:
-        db.execute("INSERT INTO books (ISBN, title, author, year) VALUES (:ISBN, :title, :author, :year)",
-        {"ISBN": ISBN, "title": title, "author": author, "year": year})
+        db.execute("INSERT INTO books (ISBN, title, author, year) VALUES (:ISBN, :title, :author, :year)", {"ISBN": ISBN, "title": title, "author": author, "year": year})
         print(f" ISBN: {ISBN}")
     db.commit()
+
 
 if __name__ == "__main__":
     main()
